671ca9ecdcc8db356b189fd0ed80bf64e0e638e6
Аноним Евгений <shadow.anonim@yandex.ru>
2024-07-24 18:50:21 +0300

    Рисование прямоугольников

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameController : MonoBehaviour
 {
     public static GameController S;
 
-    public int[,] grid;
+    public Vertex[,] grid;
     public (int, int) curValues;
+    public Vector2? startPoint = null;
 
     [SerializeField]
     private Transform gridTransform;
     [SerializeField]
     private GameObject vertexPrefab;
+    [SerializeField]
+    private GameObject rectanglePrefab;
 
     private void Awake()
     {
-        if (S == null)
+        if (S is null)
             S = this;
         else
             Debug.LogError("Second instance of Game controller!");
@@ -24,7 +29,7 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        CreateGrid(16, 16);
+        CreateGrid(32, 32);
     }
 
     public (int, int) RollDice()
@@ -36,13 +41,38 @@ public class GameController : MonoBehaviour
     public void CreateGrid(int width, int height)
     {
         Camera.main.orthographicSize = Mathf.Max(height, width / Camera.main.aspect) / 2 * 1.1f;
-        print(Camera.main.aspect);
         Vector2 startPosition = new Vector2(-width / 2, -height / 2);
-        grid = new int[width, height];
+        grid = new Vertex[width, height];
         for (int i = 0; i < width; i++)
             for (int j = 0; j < height; j++)
             {
-                Instantiate(vertexPrefab, startPosition + new Vector2(i, j), Quaternion.identity, gridTransform);
+                GameObject vertex = Instantiate(vertexPrefab, startPosition + new Vector2(i, j), Quaternion.identity, gridTransform);
+                vertex.GetComponent<Vertex>().position = startPosition + new Vector2(i, j);
             }
+    }
+
+    public void DrawRectangle(Vector2 pos, float width, float height)
+    {
+        GameObject rect = Instantiate(rectanglePrefab, pos, Quaternion.identity, gridTransform);
+
+        Transform top, bottom, left, right;
+        top = rect.transform.Find("Top");
+        bottom = rect.transform.Find("Bottom");
+        left = rect.transform.Find("Left");
+        right = rect.transform.Find("Right");
+
+        top.localScale = new Vector3(width, top.localScale.y);
+        top.localPosition = Vector3.up * height / 2;
+
+        bottom.localScale = new Vector3(width, bottom.localScale.y);
+        bottom.localPosition = Vector3.down * height / 2;
+
+        left.localScale = new Vector3(left.localScale.x, height);
+        left.localPosition = Vector3.left * width / 2;
+
+        right.localScale = new Vector3(right.localScale.x, height);
+        right.localPosition = Vector3.right * width / 2;
+
+        startPoint = null;
     }
 }
--- Assets/Scripts/Vertex.cs
+++ Assets/Scripts/Vertex.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class Vertex : MonoBehaviour
 {
-    public Vector2Int position;
+    public Vector2 position;
+
+    private void OnMouseEnter()
+    {
+        transform.localScale *= 1.5f;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale /= 1.5f;
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        print(GameController.S.startPoint + " " +  position);
+        if (GameController.S.startPoint is null)
+            GameController.S.startPoint = position;
+        else
+        {
+            Vector2 startPoint = GameController.S.startPoint.Value;
+            GameController.S.DrawRectangle(
+                (startPoint + position)/2, 
+                Mathf.Abs((startPoint - position).x), 
+                Mathf.Abs((startPoint - position).y));
+        }
+    }
 }
