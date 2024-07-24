1d8d0d15220e9608721cce675425e17c3fbca7c9
Аноним Евгений <shadow.anonim@yandex.ru>
2024-07-24 14:21:46 +0300

    Анимация панели бросков

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,19 +6,16 @@ public class GameController : MonoBehaviour
 {
     public static GameController S;
 
+    private (int, int) curValues;
+
     private void Awake()
     {
         if (S == null)
-        {
             S = this;
-        }
         else
-        {
             Debug.LogError("Second instance of Game controller!");
-        }
     }
 
-    private (int, int) curValues;
     public (int, int) RollDice()
     {
         curValues = (Random.Range(1, 7), Random.Range(1, 7));
